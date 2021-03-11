136498b13e4228e9a5527ea5344b790231bfdbba
Elaine Souza Moreira <oitodedezembro@gmail.com>
2021-03-10 21:09:29 -0300

    Definir aleatoriedade da frutinha e tamanho da cobra

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ let food = {
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16*box, 16*box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
@@ -57,7 +57,12 @@ if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+         food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
