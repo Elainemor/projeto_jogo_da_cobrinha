af4037f91c942ccb69fff5cf653f60bbaa02943e
Elaine Souza Moreira <oitodedezembro@gmail.com>
2021-03-11 00:08:01 -0300

    Correcao function Update, KeyCode para keyCode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let canvas = document.getElementById("snake");
-let context = canvas.getContext('2d');
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
@@ -15,7 +15,7 @@ let food = {
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16*box, 16*box);
 }
 
 function criarCobrinha(){
@@ -32,25 +32,25 @@ function drawFood(){
 
 document.addEventListener('keydown', update);
 
-function update (event){
-    if(event.KeyCode == 37 && direction != "right") direction = "left";
-    if(event.KeyCode == 38 && direction != "down") direction = "up";
-    if(event.KeyCode == 39 && direction != "left") direction = "right";
-    if(event.KeyCode == 40 && direction != "up") direction = "down";
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up")   direction = "down";
 }
 
 function iniciarJogo(){ 
-if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-if(snake[0].y > 15 * box && direction  == "down") snake[0].y = 0;
-if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction  == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-for(i = 1; i < snake.length; i++){
-    if(snake[0].x == snake[i].x &&  snake[0].y == snake[i].y){
-        clearInterval(jogo);
-        alert("Game Over! :(");
-    }
-}
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x &&  snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over! :(");
+        }
+    }                                                       
 
     criarBG();
     criarCobrinha();
@@ -60,14 +60,18 @@ for(i = 1; i < snake.length; i++){
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+
+    if(direction == "left")  snakeX -= box;
+
+    if(direction == "up")    snakeY -= box;
+
+    if(direction == "down")  snakeY += box;
 
     if(snakeX != food.x || snakeY != food.y){
         snake.pop();
     }
-    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+    else{
+         food.x = Math.floor(Math.random() * 15 + 1) * box;
          food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
@@ -80,4 +84,3 @@ for(i = 1; i < snake.length; i++){
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
